d4cd8226bb884b3623f75ad1a3a57e4d6f5042bd
Shin Umeda <umeda.shin@gmail.com>
2026-01-07 14:15:17 -0800

    the start of something hopefully easy

---
+++ src/app.rs
@@ -0,0 +1,78 @@
+use crate::event::{AppEvent, Event, EventHandler};
+use ratatui::{
+    DefaultTerminal,
+    crossterm::event::{KeyCode, KeyEvent, KeyModifiers},
+};
+
+#[derive(Debug)]
+pub struct App {
+    pub running: bool,
+    pub counter: u8,
+    pub events: EventHandler,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            running: true,
+            counter: 0,
+            events: EventHandler::new(),
+        }
+    }
+}
+
+impl App {
+    pub fn new() -> Self {
+        Self::default()
+    }
+    pub async fn run(mut self, mut terminal: DefaultTerminal) -> color_eyre::Result<()> {
+        while self.running {
+            terminal.draw(|frame| frame.render_widget(&self, frame.area()))?;
+            match self.events.next().await? {
+                Event::Tick => self.tick(),
+                Event::Crossterm(event) => match event {
+                    crossterm::event::Event::Key(key_event)
+                        if key_event.kind == crossterm::event::KeyEventKind::Press =>
+                    {
+                        self.handle_key_events(key_event)?
+                    }
+                    _ => {}
+                },
+                Event::App(app_event) => match app_event {
+                    AppEvent::Increment => self.increment_counter(),
+                    AppEvent::Decrement => self.decrement_counter(),
+                    AppEvent::Quit => self.quit(),
+                },
+            }
+        }
+        Ok(())
+    }
+
+    pub fn handle_key_events(&mut self, key_event: KeyEvent) -> color_eyre::Result<()> {
+        match key_event.code {
+            KeyCode::Esc | KeyCode::Char('q') => self.events.send(AppEvent::Quit),
+            KeyCode::Char('c' | 'C') if key_event.modifiers == KeyModifiers::CONTROL => {
+                self.events.send(AppEvent::Quit)
+            }
+            KeyCode::Right => self.events.send(AppEvent::Increment),
+            KeyCode::Left => self.events.send(AppEvent::Decrement),
+            // Other handlers you could add here.
+            _ => {}
+        }
+        Ok(())
+    }
+
+    pub fn tick(&self) {}
+
+    pub fn quit(&mut self) {
+        self.running = false;
+    }
+
+    pub fn increment_counter(&mut self) {
+        self.counter = self.counter.saturating_add(1);
+    }
+
+    pub fn decrement_counter(&mut self) {
+        self.counter = self.counter.saturating_sub(1);
+    }
+}
